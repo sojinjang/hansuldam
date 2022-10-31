534b351de0b44cb83591abf1f5845931af50ab30
dejaikeem <dejaikeem@gmail.com>
2022-10-31 20:38:31 +0900

    Feat: "category"

---
+++ src/db/schemas/category-schema.js
@@ -0,0 +1,16 @@
+import { Schema } from 'mongoose';
+
+const CategorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        collection: 'category',
+        timestamps: true,
+    },
+);
+
+export { CategorySchema };
