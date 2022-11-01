c4c2fa1fe6a589a7bbc1fcdfa65739c711178450
ahosang <sang0056@naver.com>
2022-11-01 23:54:31 +0900

    [feat] adminRouter 추가 , 상품기능 분리

---
+++ src/routers/admin-router.js
@@ -0,0 +1,82 @@
+import { Router } from "express";
+import is from "@sindresorhus/is";
+
+import { productService } from "../services";
+
+const adminRouter = Router();
+
+// ---- 상품관련
+// 제품 추가
+adminRouter.post("/products", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    // req (request)의 body 에서 데이터 가져오기
+    // 논의 필요
+    const { name, price, category, image, brand, content } = req.body;
+
+    // 위 데이터를 상품 db에 추가하기
+    const newProduct = await productService.addProduct({
+      name,
+      price,
+      category,
+      image,
+      brand,
+      content,
+    });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 상품 정보 수정
+adminRouter.patch("/products/:productId", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    const { productId } = req.params;
+
+    const { name, price, category, image, brand, content } = req.body;
+
+    // 위 데이터를 상품 db에 추가하기
+    const updateProduct = await productService.updateProduct(productId, {
+      name,
+      price,
+      category,
+      image,
+      brand,
+      content,
+    });
+
+    // 업데이트 이후의 데이터를 프론트에 보내 줌
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 상품 삭제
+adminRouter.delete("/products/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+
+    const products = await productService.deleteProduct(productId);
+
+    // 상품 목록(배열)을 JSON 형태로 프론트에 보냄
+    res.status(200).json(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { adminRouter };
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,85 +1,12 @@
 import { Router } from "express";
-import is from "@sindresorhus/is";
 
 import { productService } from "../services";
 
 const productRouter = Router();
 
-// 제품 추가 (관리자)
-productRouter.post("/admin/products", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "headers의 Content-Type을 application/json으로 설정해주세요"
-      );
-    }
-
-    // req (request)의 body 에서 데이터 가져오기
-    // 논의 필요
-    const { name, price, category, image, brand, content } = req.body;
-
-    // 위 데이터를 상품 db에 추가하기
-    const newProduct = await productService.addProduct({
-      name,
-      price,
-      category,
-      image,
-      brand,
-      content,
-    });
-
-    res.status(201).json(newProduct);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// 상품 정보 수정
-productRouter.patch("/admin/products/:productId", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "headers의 Content-Type을 application/json으로 설정해주세요"
-      );
-    }
-
-    const { productId } = req.params;
-
-    const { name, price, category, image, brand, content } = req.body;
-
-    // 위 데이터를 상품 db에 추가하기
-    const updateProduct = await productService.updateProduct(productId, {
-      name,
-      price,
-      category,
-      image,
-      brand,
-      content,
-    });
-
-    // 업데이트 이후의 데이터를 프론트에 보내 줌
-    res.status(200).json(updateProduct);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// 상품 삭제(관리자)
-productRouter.delete("/admin/products/:productId", async (req, res, next) => {
-  try {
-    const { productId } = req.params;
-
-    const products = await productService.deleteProduct(productId);
-
-    // 상품 목록(배열)을 JSON 형태로 프론트에 보냄
-    res.status(200).json(products);
-  } catch (error) {
-    next(error);
-  }
-});
 
 // 전체 상품 목록을 가져옴 (배열 형태)
-productRouter.get("/products", async (req, res, next) => {
+productRouter.get("/", async (req, res, next) => {
   try {
     // 전체 상품 목록을 얻음
     const products = await productService.getProducts();
@@ -92,7 +19,7 @@ productRouter.get("/products", async (req, res, next) => {
 });
 
 // 상품 상세 정보를 가져옴
-productRouter.get("/products/:productId", async (req, res, next) => {
+productRouter.get("/:productId", async (req, res, next) => {
   try {
     const { productId } = req.params;
     console.log(productId)
