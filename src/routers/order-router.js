d051e8717c12d9bec9663f3398e957dab598d728
dejaikeem <dejaikeem@gmail.com>
2022-11-03 01:52:01 +0900

    :sparkles: feat(order): add order feature
    order 기능 구현

---
+++ src/routers/order-router.js
@@ -0,0 +1,174 @@
+import { Router } from "express";
+import is from "@sindresorhus/is";
+
+import { orderService } from "../services";
+
+const orderRouter = Router();
+
+// 주문 추가 - userId false로 설정하면 선택으로 들어가고 안들어가고가 될듯?
+orderRouter.post("/", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+    // req (request)의 body 에서 데이터 가져오기
+    const {
+        fullName,
+        address,
+        shipping,
+        paymentMethod,
+        paymentDetail,
+        priceSum,
+        productList,
+        userId,
+        phoneNumber,
+    } = req.body;
+    // 위 데이터를 주문 db에 추가하기
+    const newOrder = await orderService.addOrder({
+        fullName,
+        address,
+        shipping,
+        paymentMethod,
+        paymentDetail,
+        priceSum,
+        productList,
+        userId,
+        phoneNumber,
+    });
+
+    res.status(201).json(newOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 주문 수정 구매자
+orderRouter.patch("/:orderId", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    const { orderId } = req.params;
+
+    const {
+        fullName,
+        productList,
+        phoneNumber,
+        address,
+        paymentMethod,
+        paymentDetail,
+    } = req.body;
+
+    // 위 데이터를 카테고리 db에 추가하기
+    const updateOrder = await orderService.updateOrder(orderId, {
+        fullName,
+        productList,
+        phoneNumber,
+        address,
+        paymentMethod,
+        paymentDetail,
+    });
+    res.status(200).json(updateOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 주문 수정 관리자
+orderRouter.patch("/admin/orders/:orderId", async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    const { orderId } = req.params;
+
+    const {
+        fullName,
+        productList,
+        phoneNumber,
+        address,
+        paymentMethod,
+        paymentDetail,
+        status
+    } = req.body;
+
+    // 위 데이터를 카테고리 db에 추가하기
+    const updateOrder = await orderService.updateOrderAdmin(orderId, {
+        fullName,
+        productList,
+        phoneNumber,
+        address,
+        paymentMethod,
+        paymentDetail,
+        status,        
+    });
+
+    // 업데이트 이후의 데이터를 프론트에 보내 줌
+    res.status(200).json(updateOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 주문 취소 구매자
+orderRouter.delete("/:orderId", async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+
+    const order = await orderService.deleteOrder(orderId);
+
+    res.status(200).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 주문 삭제(관리자)
+orderRouter.delete("/admin/orders/:orderId", async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+
+    const order = await orderService.deleteOrder(orderId);
+
+    res.status(200).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 전체 주문 목록을 가져옴 -안쓰일건지?
+orderRouter.get("/", async (req, res, next) => {
+  try {
+    // 전체 주문 목록을 얻음
+    const order = await orderService.getOrders();
+
+    // 주문 목록(배열)을 JSON 형태로 프론트에 보냄
+    res.status(200).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 주문 상세 정보를 가져옴 
+orderRouter.get("/:orderId", async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+
+    const order = await orderService.getOrderById(orderId);
+
+    // 주문 목록(배열)을 JSON 형태로 프론트에 보냄
+    res.status(200).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { orderRouter };
---
+++ src/services/order-service.js
@@ -0,0 +1,91 @@
+import { orderModel } from "../db";
+
+class OrderService {
+  constructor(orderModel) {
+    this.orderModel = orderModel;
+  }
+
+//주문 추가 // 여기에 user id false로 하고 주문을 하면 구분이 가능할듯?
+  async addOrder(orderInfo) {
+    // db에 저장
+    const createdNewOrder = await this.orderModel.create(orderInfo);
+    return createdNewOrder;
+  }
+
+  async getOrders() {
+    const orders = await this.orderModel.findAll();
+    return orders;
+  }
+
+  async getOrderById(orderId) {
+    const order = await this.orderModel.findById(orderId);
+    return order;
+  }
+
+ // 관리자 주문 상태 수정
+ async updateOrderAdmin(orderId, toUpdate) {
+    // 우선 해당 id의 상품이 db에 있는지 확인
+    let order = await this.orderModel.findById(orderId);
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!order) {
+      res.status(404);
+      throw new Error("일치하는 카테고리가 없습니다. 다시 한 번 확인해 주세요.");
+    }
+    
+    // 업데이트 진행
+    order = await this.orderModel.update({
+      orderId,
+      update: toUpdate,
+    });
+
+    return order;
+  }
+
+
+ //구매자 주문 수정
+  async updateOrder(orderId, toUpdate) {
+    let order = await this.orderModel.findById(orderId);
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!order) {
+      res.status(404);
+      throw new Error("일치하는 주문 내역이 없습니다. 다시 한 번 확인해 주세요.");
+    }
+//findbyid
+    if (order.status !== "상품준비중"){
+        res.status(404);
+        throw new Error("상품이 배송중이기에 고객센터에 문의 부탁드립니다.");
+    }
+    
+    // 업데이트 진행
+    order = await this.orderModel.update({
+      orderId,
+      update: toUpdate,
+    });
+
+    return order;
+  }
+
+//주문 취소
+
+
+//관리자 주문 삭제
+  async deleteOrder(orderId) {
+    // 우선 해당 id의 상품이 db에 있는지 확인
+    let order = await this.orderModel.findById(orderId);
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!order) {
+      res.status(404);
+      throw new Error("일치하는 주문 내역이 없습니다. 다시 한 번 확인해 주세요.");
+    }
+
+    // 업데이트 진행
+    const deletedOrder = await this.orderModel.delete({orderId});
+    return deletedOrder;
+  }
+}
+
+const orderService = new OrderService(orderModel);
+
+export { orderService };
