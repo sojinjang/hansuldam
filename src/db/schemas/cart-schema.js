7c702b3c4b7c59467fb2ba41f8fcfb57557c440d
ahosang <sang0056@naver.com>
2022-11-04 18:51:34 +0900

    :truck: rename: 변수명 받기

--- src/db/schemas/cart-schema.js
+++ src/db/schemas/cart-schema.js
@@ -2,28 +2,11 @@ import { Schema } from "mongoose";
 
 const CartSchema = new Schema(
   {
-    userId: {
-      type: String,
-      required: false,
-    },
-    productsInCart: {
-      type: [
-        new Schema(
-          {
-            id: { type: String, required: true },
-            quantity: { type: Number, default: 1, required: true },
-          },
-          {
-            _id: false,
-          }
-        ),
-      ],
-      required: false,
-    },
+    id: { type: String, required: true },
+    quantity: { type: Number, default: 1, required: true },
   },
   {
-    collection: "carts",
-    timestamps: true,
+    _id: false,
   }
 );
 
