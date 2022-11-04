971ab55153aefd2373497e2ae297cc7de60ea354
HelloJianii <ssjian123@gmail.com>
2022-11-04 17:27:39 +0900

    :sparkles: feat: "최종 회원가입 버튼 기능구현"
    상세 정보 페이지에 정보들을 다 입력해야만 가입 버튼이 동작함

--- src/views/join-membership/join-success.js
+++ src/views/join-membership/join-success.js
@@ -3,117 +3,139 @@ import footer from "../template/footer/footer.js";
 
 const body = document.querySelector(".body-container");
 const main_form = document.querySelector(".body-join-form");
-const numberInput = document.querySelector("#userNumberInput");
-const emailInput = document.querySelector("#emailInput");
-const checkBtn = document.querySelector(".checkBtn");
-const name = document.querySelector("#nameInput");
+const ageInput = document.querySelector("#userAgeInput");
+let nameInput = document.querySelector("#userNameInput");
+const checkBtn = document.querySelector(".adultCheckButton");
+const email = document.querySelector("#email");
+let name = document.querySelector("#nameInput");
 const password = document.querySelector("#passwordInput");
 const passwordCheck = document.querySelector("#passwordCheck");
-const addressCode = document.querySelector(".addressCode");
-const addressLocation = document.querySelector(".addressLocation");
-const addressDetail = document.querySelector(".addressDetail");
+const addressPostalCode = document.querySelector("#addressPostalCode");
+const addressLocation = document.querySelector("#addressLocation");
+const addressDetail = document.querySelector("#addressDetail");
 const phoneNumber = document.querySelector("#phoneNumber");
 const joinPassBtn = document.querySelector(".join-form-button");
+
+const receivedData = location.href.split('?')[1];
+console.log(receivedData);
 
 body.insertAdjacentHTML("afterend", footer());
 
 // 성인인증하기 버튼 이벤트 리스너
-checkBtn.addEventListener("click", joinPage);
+checkBtn.addEventListener("click", OpenjoinPage);
 
-function joinPage (e) {
-    e.preventDefault();
+function OpenjoinPage() {
+
     this.onclick = null;
-    //main 태그에 인증완료시 나오는 엘리먼트 삽입하기
-    // if (main_form.style.display == "none"){
-    // } 왜 안되냐!!!!
-    main_form.style.display = "flex";
+    const ageinput = ageInput.value;
+
+
+    function isAdult(ageinput) {
+        const twenty = 20;
+
+        if (Number(ageinput) >= twenty) {
+            return true;
+        }
+        return false;  
+    }
+
+    if (isAdult(ageinput)) {
+        alert("인증 성공하셨습니다.")
+        main_form.style.display = "flex";
+        return;
+    }
+    if (!isAdult(ageinput)) {
+        alert("미성년자는 가입 불가입니다.")
+        //window.location.href="http://127.0.0.1:5500/src/views/home/home.html?%22"
+    }
 }
 
-// 생년월일로 만나이 계산하기
-const dateObj = new Date(); // 날짜 Object 생성
-const age = 0; // 나이
-const yy = dateObj.getFullYear(); // 현재년도
-const mm = dateObj.getMonth() + 1; // 현재월
-const dd = dateObj.getDate(); // 현재일
-
-
-
-// 인증 실패
-//if(numberInput.value )
-// 인증 완료
-
-// 상세 페이지 이메일 형식 확인
-
-//이메일 정규식 체크 함수
-// function email_check(email) {
-// 	let reg_email = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-
-//     if(!reg_email.test(str)) {                            
-//         return false;         
-//         }else {                       
-//         return true;         
-//         }
-// }
-
-// function checkIt() {
-//     let email = emailInput.value
-//     let exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-
-//     if(exptext.test(email) == false) {
-
-//         //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우			
-
-//         alert("이메일형식이 올바르지 않습니다.");
-
-//         return false;
-//     }
-// }
+// 인증 완료 시, 인증창의 이름, 이메일 속성을  가입 상세페이지의 이름, 이메일 속성으로 추가하기
+// 약간 간편가입 페이지의 버튼 이벤트리스너에 넣어야 할 것 같음. 그렇다면 두 속성을 모두 간편가입 페이지에서 들고와야할듯
+// 이메일 속성은 join-membership.html에서 가져와야함.
+// 이름과, 이메일 속성 변수로 할당하기
+// 이름 = "join-membership.html/#nameInput"
+// 간편가입이름 = document.querySelector(join-membership.html/#nameInput).innerText
+// 이메일 = "join-membership.html/#emailInput"
+// 간편가입이메일 = document.querySelector(join-membership.html/#emailInput).innerText
+// 간편 로그인 페이지에서 리스너가 실행되면 
+// 두 dom요소의 속성값을 상세가입 페이지의 이름, 이메일 요소의 속성값으로 추가한다.
 
 
 
 // 가입 완료 버튼 이벤트리스너
-joinPassBtn.addEventListener("click", joinSuccess);
+joinPassBtn.addEventListener("click", successJoin);
 
-function joinSuccess(e) {
+function successJoin(e) {
     e.preventDefault();
 
-    const InputName = name.value;
-    const InputPassword = password.value;
-    const InputPasswordCheck = passwordCheck.value;
-    //const InputAddressCode = addressCode.value;
-    const InputAddressLocation = addressLocation;
-    const InputAddressDetail = addressDetail;
-    const InputPhoneNumber = phoneNumber.value;
-
-    //const res = await fetch("api주소");
-
-    // 이메일 형식이 맞는지
-    function verifyEmail() {
-        // 이메일 검증 스크립트 작성
-        let emailVal = emailInput.value;
+    const inputName = name.value;
+    const inputPassword = password.value;
+    const inputPasswordCheck = passwordCheck.value; // 변수랑 함수의 이름이 똑같음
+    const inputAddressPostalCode = addressPostalCode.value;
+    const inputAddressLocation = addressLocation.value;
+    const inputAddressDetail = addressDetail.vaule;
+    const inputPhoneNumber = phoneNumber.value;
     
-        var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-        // 검증에 사용할 정규식 변수 regExp에 저장
-        
-        if (emailVal.match(regExp) != null) {
-            alert('Good!');
-        }
-        else {
-            alert('Error');
-        }
-    };
-
-    verifyEmail();
-    // 중복 이메일이 아닌지
-
-
     // 비밀번호와 비밀먼호 확인이 일치하는지
-    if(InputPassword !== res.password){
-        alert("비밀번호가 일치하지 않습니다.")
+    function checkPassword(password){
+        if(password == inputPassword) {
+            return true;
+        }
+        if(password !== inputPassword) {
+            console.log("비밀번호가 일치하지 않습니다.")
+            return false;
+        }
     }
 
-    // 입력한 값을 res.json형태로 db로 보낸다.
-    const userInputData = {InputName, InputPassword, InputPasswordCheck, InputAddressCode, InputAddressLocation, InputAddressDetail, InputPhoneNumber};
+    // 주소값 3곳 다 입력되어 있는지
+    function checkAddress(address, address1, address2){
+        if(address == "" || address1 == "" || address2 == "") {
+            console.log("주소를 다 입력해주세요")
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 휴대폰번호 입력 했는지
+    function checkPhonNumber(phoneNumber) {
+        if(phoneNumber == "") {
+            console.log("휴대폰 번호를 입력하세요");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 회원가입 전체 정보가 다 확인 됐는지 확인
+    function checkTotal(password, address, address1, address2, phoneNumber) {
+        if(checkPassword(password) && checkAddress(address, address1, address2) && checkPhonNumber(phoneNumber)) {
+            console.log("가입성공!")
+            window.location.href="http://127.0.0.1:5500/src/views/home/home.html?%22";
+            return true;
+            
+        } else {
+            console.log("입력칸을 다시 확인해주세요")
+        }
+        console.log("작동이 되는가?")
+    }
+
+    checkTotal(inputPasswordCheck, inputAddressPostalCode, inputAddressLocation, inputAddressDetail, inputPhoneNumber);
+    
+
+    // 입력한 값을 res.json형태로 변환
+    const userInputData = {
+        fullName: inputName,
+        password: inputPassword,
+        address: {
+            PostalCode: inputAddressPostalCode,
+            address1: inputAddressLocation,
+            address2: inputAddressDetail,
+        },
+        phoneNumber: inputPhoneNumber,
+    };
+    //const res = await fetch("api주소");
     JSON.stringify(userInputData);
 }
 
