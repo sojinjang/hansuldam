908373d92a933c63b1897fbd70fe15f83c6a869e
wooooooongs <pires11233@gmail.com>
2022-11-04 15:48:40 +0900

    :sparkles: feat: add code for get data from DB

--- src/views/product_detail/product_detail.js
+++ src/views/product_detail/product_detail.js
@@ -1,5 +1,31 @@
-// 예제 데이터
-const data = '';
+async function fetchData() {
+	const res = await fetch('http://localhost:8900/api/products', {
+		method: 'GET',
+	});
+
+	return await res.json();
+}
+
+async function getData() {
+	try {
+		const data = await fetchData();
+
+		console.log(items);
+		items.forEach((v, i) => {
+			const value = items[i].className.split(' ')[1].split('content__')[1];
+			
+			if (data[0][value] !== undefined) {
+				v.innerHTML = data[0][value];
+			} else {
+				v.innerHTML = '정보가 없습니다. 디비 스키마 확인 혹은 html 클래스명 확인하셈~!';
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
+	
+}
+
 
 const items = document.querySelectorAll('.content__item');
 const orderButton = document.querySelector('#order-button');
@@ -9,18 +35,13 @@ orderButton.addEventListener('click', clickOrder);
 basketButton.addEventListener('click', clickBasket);
 
 function clickOrder() {
-	alert('주문 페이지로 이동합니다...');
-	window.location.href = '/orders';
+	console.log('주문 페이지로 이동합니다...');
+	// window.location.href = '/orders';
 }
 
 function clickBasket() {
-	alert('장바구니에 담았습니다!');
+	console.log('장바구니에 담았습니다!');
 }
 // class명과 일치하는 data값 불러온 후 적용하기
-items.forEach((v, i) => {
-	const value = items[i].className
-		.split(' ')[1]
-		.split('content__')[1];
 
-	// v.innerHTML = data[value];
-});
+getData();
