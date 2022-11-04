2518500c1c11c0e7876f89a828bd4b9fc2f3c716
wooooooongs <pires11233@gmail.com>
2022-11-04 16:06:52 +0900

    :recycle: refactor: separate template code from page

--- src/views/category_page/getHeaderFooter.js
+++
@@ -1,7 +0,0 @@
-import footer from "../template/footer/footer.js";
-import header from "../template/header/header.js";
-
-const main = document.querySelector(".body-container");
-
-main.insertAdjacentHTML("beforebegin", header());
-main.insertAdjacentHTML("afterend", footer());
