83385e916d03b30864029bd72a859f47aff46e43
sojinjang <jsojin6119@gmail.com>
2022-11-05 13:58:31 +0900

    :triangular_flag_on_post: test: remove test code using form
    제품 상세 페이지에서 데이터를 localStorage에 저장하는 기능 구현이 끝났기 때문에 장바구니 페이지에서 form 입력값으로 데이터를 localStorage에 저장해보는 테스트 코드를 삭제함.

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -6,56 +6,18 @@ const main = document.querySelector(".body-container");
 main.insertAdjacentHTML("beforebegin", header());
 main.insertAdjacentHTML("afterend", footer());
 
-const productForm = document.getElementById("product-form");
-const productInput = document.querySelector("#product-form input");
 const shoppingbagList = document.querySelector(".shoppingbag-list");
 
 const PRODUCTS_KEY = "products";
 
-let productArr = [
-  {
-    _id: "249ee1e45022fa608b63e994",
-    category: "증류주",
-    brand: "전주이강주",
-    name: "조정형 명인 전주 이강주",
-    price: 5500,
-    volume: 375,
-    quantity: 200,
-    img: "../img/redmonkey.jpeg",
-    sold: 10,
-    alcoholType: "증류",
-    alcoholDegree: 19,
-    manufacturedDate: "2022-04-15",
-    createdAt: "2022-06-07T05:28:04.709Z",
-    updatedAt: "2022-06-07T05:32:19.548Z",
-  },
-  {
-    _id: "249ee1e45022fa608b63e994",
-    category: "증류주",
-    brand: "전주이강주",
-    name: "조정형 명인 전주 이강주",
-    price: 5500,
-    volume: 375,
-    quantity: 200,
-    img: "../img/redmonkey.jpeg",
-    sold: 10,
-    alcoholType: "증류",
-    alcoholDegree: 19,
-    manufacturedDate: "2022-04-15",
-    createdAt: "2022-06-07T05:28:04.709Z",
-    updatedAt: "2022-06-07T05:32:19.548Z",
-  },
-];
-
 function saveProducts() {
-  localStorage.setItem(PRODUCTS_KEY, JSON.stringify(productArr));
+  localStorage.setItem(PRODUCTS_KEY, JSON.stringify(tempData));
 }
 
 function deleteProduct(event) {
   const productInfoDiv = event.target.parentElement;
   const productDiv = productInfoDiv.productInfoDiv;
   productDiv.remove();
-  products = products.filter((product) => product.id !== parseInt(li.id));
   saveProducts();
 }
 
@@ -64,13 +26,9 @@ function showProducts(item) {
   product = document.createElement("div");
   product.setAttribute("class", "product");
   product.innerHTML = `<div class="checkbox-wrapper">
-                <button type="button" class="custom-checkbox">
-                  <img
-                    class="checkbox-img"
-                    src="../img/check.png"
-                    alt="checkbox"
-                  />
-                </button>
+                <input type="checkbox" name="checker" id=buy-checker /><label
+                  for="checker1"
+                ></label>
               </div>
               <div class="product-info-top">
                 <div class="thumbnail">
@@ -79,7 +37,7 @@ function showProducts(item) {
                 <div class="product-info">
                   <div class="product-brand">${item.brand}</div>
                   <div class="product-name">${item.name}</div>
-                  <div class="product-volume">${item.volume}</div>
+                  <div class="product-volume">${item.volume}ml</div>
                 </div>
                 <button type="button" class="product-remove-button">
                   <img
@@ -112,93 +70,66 @@ function showProducts(item) {
                     </button>
                   </div>
                 </div>
-                <div class="price">${item.price}</div>
+                <div class="price">${item.price}원</div>
               </div>`;
   shoppingbagList.append(product);
 }
 
-// let productArr = [];
-
-function testShowProducts(productObj) {
-  let product = undefined;
-  product = document.createElement("div");
-  product.setAttribute("class", "product");
-  product.innerHTML = `<div class="checkbox-wrapper">
-                <button type="button" class="custom-checkbox">
-                  <img
-                    class="checkbox-img"
-                    src="../img/check.png"
-                    alt="checkbox"
-                  />
-                </button>
-              </div>
-              <div class="product-info-top">
-                <div class="thumbnail">
-                  <img class="product-img" src="../img/redmonkey.jpeg" />
-                </div>
-                <div class="product-info">
-                  <div class="product-brand">배상면주가</div>
-                  <div class="product-name">${productObj}</div>
-                  <div class="product-volume">375ml</div>
-                </div>
-                <button type="button" class="product-remove-button">
-                  <img
-                    class="remove-img"
-                    src="../img/x.png"
-                    alt="remove-button"
-                  />
-                </button>
-              </div>
-              <div class="blank"></div>
-              <div class="product-info-bottom">
-                <div class="amount-control">
-                  <div class="decrease">
-                    <button class="minus-button" type="button">
-                      <img
-                        class="minus-image"
-                        src="../img/minus.png"
-                        alt="minus"
-                      />
-                    </button>
-                  </div>
-                  <div class="amount">1</div>
-                  <div class="increase">
-                    <button class="plus-button" type="button">
-                      <img
-                        class="plus-image"
-                        src="../img/plus.png"
-                        alt="plus"
-                      />
-                    </button>
-                  </div>
-                </div>
-                <div class="price">7900원</div>
-              </div>`;
-  shoppingbagList.append(product);
-}
-
-function handleProductSubmit(event) {
-  event.preventDefault();
-  const newProduct = productInput.value;
-  productInput.value = "";
-  const newProductObj = {
-    text: newProduct,
-    id: Date.now(),
-  };
-  // productArr.push(newProductObj);
-  testShowProducts(newProduct);
-  // showProducts(productArr);
-  saveProducts();
-}
-
-productForm.addEventListener("submit", handleProductSubmit);
+let tempData = [
+  {
+    _id: "249ee1e45022fa608b63e994",
+    category: "증류주",
+    brand: "전주이강주",
+    name: "조정형 명인 전주 이강주",
+    price: 5500,
+    volume: 375,
+    quantity: 1,
+    img: "../img/redmonkey.jpeg",
+    sold: 10,
+    alcoholType: "증류",
+    alcoholDegree: 19,
+    manufacturedDate: "2022-04-15",
+    createdAt: "2022-06-07T05:28:04.709Z",
+    updatedAt: "2022-06-07T05:32:19.548Z",
+  },
+  {
+    _id: "5555",
+    category: "청주",
+    brand: "토박이",
+    name: "한산 소곡주",
+    price: 11000,
+    volume: 500,
+    quantity: 3,
+    img: "../img/redmonkey.jpeg",
+    sold: 13,
+    alcoholType: "청주",
+    alcoholDegree: 16,
+    manufacturedDate: "2022-04-15",
+    createdAt: "2022-06-07T05:28:04.709Z",
+    updatedAt: "2022-06-07T05:32:19.548Z",
+  },
+  {
+    _id: "1234",
+    category: "증류주",
+    brand: "제이팍",
+    name: "원소주",
+    price: 25000,
+    volume: 375,
+    quantity: 2,
+    img: "../img/redmonkey.jpeg",
+    sold: 5,
+    alcoholType: "증류주",
+    alcoholDegree: 21,
+    manufacturedDate: "2022-11-05",
+    createdAt: "2022-11-07T05:28:04.709Z",
+    updatedAt: "2022-11-07T05:32:19.548Z",
+  },
+];
+saveProducts();
 
 const savedProducts = localStorage.getItem(PRODUCTS_KEY);
 
 if (savedProducts !== null) {
   const parsedProducts = JSON.parse(savedProducts);
-  console.log(parsedProducts);
-  console.log(parsedProducts[0].price);
-  productArr = parsedProducts;
   parsedProducts.forEach(showProducts);
 }
