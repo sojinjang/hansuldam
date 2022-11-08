567c5326aa7debbc8697c6f2984d8c8a03d884f8
wooooooongs <pires11233@gmail.com>
2022-11-09 03:37:35 +0900

    :art: style: update codes with prettier
    오더페이지도 추가

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -1,23 +1,24 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 
 const viewsRouter = express.Router();
 
 // 페이지별로 html, css, js 파일들을 라우팅함
 // 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
 // http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
-viewsRouter.use("/", serveStatic("home"));
-viewsRouter.use("/join", serveStatic("join-membership"));
-viewsRouter.use("/login", serveStatic("login"));
-viewsRouter.use("/order", serveStatic("order"));
-viewsRouter.use("/cart", serveStatic("cart"));
+viewsRouter.use('/', serveStatic('home'));
+viewsRouter.use('/join', serveStatic('join-membership'));
+viewsRouter.use('/login', serveStatic('login'));
+viewsRouter.use('/order', serveStatic('order'));
+viewsRouter.use('/cart', serveStatic('cart'));
 viewsRouter.use('/products', serveStatic('category_page'));
-viewsRouter.use("/product-detail", serveStatic("product_detail"));
+viewsRouter.use('/product-detail', serveStatic('product_detail'));
 viewsRouter.use('/event-page', serveStatic('event_page'));
 viewsRouter.use('/admin', serveStatic('admin'));
+viewsRouter.use('/order-page', serveStatic('order_page'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
-viewsRouter.use("/", serveStatic(""));
+viewsRouter.use('/', serveStatic(''));
 
 // views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
 // 이 때 ${resource}.html 을 기본 파일로 설정함.
