bed36c2f48127d0791bb60f0a5b727b63b762fd2
sojinjang <jsojin6119@gmail.com>
2022-11-08 16:08:27 +0900

    :sparkles: feat(utils): get or set cookie

---
+++ src/views/utils/cookie.js
@@ -0,0 +1,12 @@
+const setCookie = (key, value) => {
+  document.cookie = key + "=" + JSON.stringify(value) + "; max-age=3600";
+};
+
+const getCookie = (key) => {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" + key.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+};
