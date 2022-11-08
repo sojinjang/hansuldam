0a1e5d6d349934d0a9b2cdaafdda088caa65caca
sojinjang <jsojin6119@gmail.com>
2022-11-08 17:53:54 +0900

    :bug: debug(login): fix getting response problem, enable to save token in cookie

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,69 +1,52 @@
+import { setCookie } from "../utils/cookie.js";
+
 const emailInput = document.querySelector("#emailInput");
 const passwordInput = document.querySelector("#passwordInput");
 const loginBtn = document.querySelector("#submitButton");
 
-loginBtn.addEventListener("click", handleSubmit);
+const TOKEN = "token";
 
-async function handleSubmit(e){
+async function logIn(e) {
   e.preventDefault();
 
   const email = emailInput.value;
   const password = passwordInput.value;
-  const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-  // 임시 데이터
-  const data = {
-    email: "elice@test.com",
-    password: 1234
-  }
-  
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-  // 이메일 입력할 때 유효성 검사하기
-  // 이메일을 형식에 맞게 잘 입력했는지?
-
-  if(email.match(emailRegExp) == null) {
-    alert("이메일 형식을 다시 확인해주세요.")
+  if (email.match(emailRegExp) == null) {
+    alert("이메일 형식을 다시 확인해주세요.");
     return;
   }
 
-  if(password.length < 4) {
-    alert("비밀번호 4자리 이상 입력해주세요")
+  if (password.length < 4) {
+    alert("비밀번호 4자리 이상 입력해주세요");
     return;
   }
 
   const inputData = {
     email: email,
-    password: password
+    password: password,
   };
 
   try {
-    const result = await fetch("/api/user/login", {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(inputData),
     });
-  
-    const { token } = result;
-  
-    sessionStorage.setItem("token", token);
-  } catch(err) {
+    const token = await response.json();
+    setCookie(TOKEN, token);
+    sessionStorage.setItem("token", JSON.stringify(token));
+  } catch (err) {
     console.error(err.stack);
-    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-
+    alert(err.errorMessage);
   }
-  
-  var n = sessionStorage.getItem("token");
-  console.log(n);
-  console.log(token);
-
-
-  // const { token } = result;
-    
-  // SessionStorage.setItem("token", token);
-    
-    // 토큰을 쿠키에 저장
 }
 
 // 회원가입 버튼을 눌렀을 때 버튼 태그에 링크 걸어둠
 // 비밀번호 찾기 버튼 눌렀을 때 버튼 태그에 링크 걸어둠
+
+loginBtn.addEventListener("click", logIn);
