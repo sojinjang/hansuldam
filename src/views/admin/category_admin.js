4381e0fc1439f46e5f5a4fc16a7d92f75dace94c
wooooooongs <pires11233@gmail.com>
2022-11-09 23:10:40 +0900

    :recycle: refactor: update codes for category admin page
    가독성을 위해 함수 순서를 바꾸고, api.js를 활용하는 등 간결한 코드를 만드려 노력했습니다. 또한 가독성을 위해 초기화 함수인 initFunc 함수를 추가하였습니다.

--- src/views/admin/category_admin.js
+++ src/views/admin/category_admin.js
@@ -2,40 +2,87 @@ import { get, post, patch, delete as del } from '../api.js'
 
 const $ = (selector) => document.querySelector(selector);
 
-async function fetchCategory() {
-  const res = await fetch('/api/category/', {
-    method: 'GET',
-  });
+function initFunc() {
+  $('.category-menu').addEventListener('click', () => {
+    const productsContainer = document.querySelector('.products-container');
+    const categoriesContainer = document.querySelector('.categories-container');
+    const addCategoryModal = document.querySelector('.add-category-modal');
+    const addProductModal = document.querySelector('.add-product-modal');
 
-  return await res.json();
+    if (productsContainer) { productsContainer.remove() };
+    if (categoriesContainer) { categoriesContainer.remove(); };
+    if (addProductModal) { addProductModal.remove() };
+    if (addCategoryModal) { addCategoryModal.remove() };
+    
+    openCategoryMenu();
+  });
 }
 
-async function showCategories() {
-  let categoriesData = await fetchCategory();
-
-  $('.category-menu').addEventListener('click', async () => {
-    if ($('.products-container')) $('.products-container').remove();
-    if (!$('.categories-container')) {
-      $('.category-menu').classList.add('isClicked');
+async function openCategoryMenu() {
+  const categoriesData = await get('/api/category');
+  $('.category-menu').classList.add('isClicked');
   
-      const productContainerHTML = `<section class="categories-container">
-  <button class="button close-button">닫기</button>
+  const productContainerHTML = `<section class="categories-container">
   <button class="button add-button">추가</button>
-  <div class="columns title-container">
-    <div class="column is-2 row-name">카테고리명</div>
-    <div class="column is-2 row-products">항목</div>
-  </div>
-  </section>`;
-  
-      await $('.admin-menu').insertAdjacentHTML('afterend', productContainerHTML);
-      categoriesData.forEach(async (category, index) => {
-        await renderCategory(category);
-        if (index == categoriesData.length - 1) deleteCategory();
-      });
-      $('.close-button').addEventListener('click', closeSection);
-      $('.add-button').addEventListener('click', addCategory);
+  <button class="button close-button">닫기</button>
+<div class="columns title-container">
+  <div class="column is-2 row-name">카테고리명</div>
+  <div class="column is-2 row-products">항목</div>
+</div>
+</section>`;
+
+  await $('.admin-menu').insertAdjacentHTML('afterend', productContainerHTML);
+  categoriesData.forEach(async (category, index) => {
+    await renderCategory(category);
+    if (index == categoriesData.length - 1) {
+      modifyCategory();
+      deleteCategory();
     }
   });
+
+  $('.close-button').addEventListener('click', closeSection);
+  $('.add-button').addEventListener('click', addCategory);
+};
+
+function closeSection() {
+  $('.category-menu').classList.remove('isClicked');
+  $('.categories-container').remove();
+}
+
+function addCategory() {
+  $('.add-button').classList.add('none');
+  $('.close-button').classList.add('none');
+
+  const categoryModalHtml = `<label class="add-category-modal">
+  <input class="input is-rounded category-input" type="text" name="name" placeholder="추가 할 카테고리 이름을 입력하세용" />
+  <button class="button add-category-button">추가</button>
+  <button class="button close-modal-button">닫기</button>
+</label>`;
+  $('.admin-menu').insertAdjacentHTML('afterend', categoryModalHtml);
+
+  $('.add-category-button').addEventListener('click', async () => {
+    const inputValue = $('.category-input').value;
+
+    await postCategory(inputValue);
+
+    $('.add-category-modal').remove();
+    refreshData();
+  });
+
+  $('.close-modal-button').addEventListener('click', () => {
+    $('.add-category-modal').remove();
+    refreshData();
+  });
+
+
+}
+
+async function postCategory(inputValue) {
+  const inputValueObject = {
+    name: inputValue
+  }
+
+  await post('/api/admin/category' ,inputValueObject);
 }
 
 async function renderCategory(category) {
@@ -49,103 +96,59 @@ async function renderCategory(category) {
   categorySection.setAttribute('id', _id);
   categorySection.innerHTML = `<div class="column is-2 row-name">${name}</div>
 <div class="column is-8 row-products">${products}</div>
-<div class="column is-1"><button id="${_id}" class="button column detail-button">상세</button></div>
+<div class="column is-1"><button id="${_id}" class="button column modify-button">수정</button></div>
 <div class="column is-1"><button id="${_id}" class="button column delete-button">삭제</button></div>
 `;
 
   $('.categories-container').append(categorySection);
-}
+};
 
-function closeSection() {
-  $('.category-menu').classList.remove('isClicked');
-  $('.categories-container').remove();
-  $('.add-category-modal').remove();
-}
-
-async function addCategory() {
-  const categoryModalHtml = `<div class="add-category-modal">
-  <input class="input is-rounded category-input" type="text" name="name" placeholder="추가 할 카테고리 이름을 입력하세용" />
-  <div clas>
-    <button class="button add-category-button">항목 추가</button>
-    <button class="button close-modal-button">닫기</button>
-  </div>
-</div>`;
-  $('.admin-menu').insertAdjacentHTML('afterend', categoryModalHtml);
-  $('.add-category-button').addEventListener('click', async () => {
-    const inputValue = $('.category-input').value;
-    const resData = await postCategory(inputValue);
-    console.log(resData);
-  });
-
-  $('.close-modal-button').addEventListener('click', () => {
-    $('.add-category-modal').remove();
-  });
-}
-
-async function postCategory(inputValue) {
-  const inputValueObject = {
-    name: inputValue
-  }
-
-  await post('/api/admin/category' ,inputValueObject);
-}
-
-async function deleteCategory() {
+function deleteCategory() {
   const deleteBtn = document.querySelectorAll('.delete-button');
   deleteBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const currentId = e.target.getAttribute('id');
+
+      e.target.setAttribute('class', 'button column is-danger delete-button-confirm');
+
+      $('.delete-button-confirm').addEventListener('click', async () => {
+        await del('/api/admin/category', currentId);
+        refreshData();
+      });
+    });
+  });
+};
+
+function modifyCategory() {
+  const modifyBtn = document.querySelectorAll('.modify-button');
+  modifyBtn.forEach((button) => {
     button.addEventListener('click', async (e) => {
       const currentId = e.target.getAttribute('id');
 
-      await del('/api/admin/category', currentId);
+      const categoryModifyModalHtml = `<label class="modify-category-modal">
+  <input class="input is-rounded modify-category-input" type="text" name="name" placeholder="카테고리 이름을 입력하세요." />
+  <button class="button modify-category-button">추가</button>
+  <button class="button close-modify-button">닫기</button>  
+</label>`;
+      await e.currentTarget.parentNode.parentNode.insertAdjacentHTML('beforebegin', categoryModifyModalHtml);
+
+      $('.close-modify-button').addEventListener('click', () => {
+        $('.modify-category-modal').remove();
+      });
+      
+      $('.modify-category-button').addEventListener('click', async () => {
+        const modifyValue = { name: $('.modify-category-input').value };
+        await patch('/api/admin/category', currentId, modifyValue);
+        $('.categories-container').remove();
+      });
+
     });
   });
-}
+};
 
-// async function renderDetailCategory() {
-//   const productsData = await fetchData();
-//   const detailBtn = document.querySelectorAll('.detail-button');
-//   const delBtn = document.querySelectorAll('.delete-button');
+function refreshData() {
+  $('.categories-container').remove();
+  openCategoryMenu();
+};
 
-//   detailBtn.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//       let currentId = e.target.getAttribute('id');
-//       const currentIndex = productsData.findIndex(
-//         (product) => product._id === currentId
-//       );
-//       const currentData = productsData[currentIndex];
-//       const {
-//         _id,
-//         stock,
-//         brand,
-//         description,
-//         sales,
-//         manufacturedData,
-//         alcoholDegree,
-//       } = currentData;
-//       const detailHtml = `<div class="columns items-container items-detail" id="cls${_id}">
-//       <div class="column is-2">${stock}</div>
-//       <div class="column is-2">${brand}</div>
-//       <div class="column is-2">${description}</div>
-//       <div class="column is-2">${sales}</div>
-//       <div class="column is-2">${manufacturedData}</div>
-//       <div class="column is-1">${alcoholDegree}</div>
-//       <div class="column is-1">
-//       <button class="button column">수정</button>
-//       </div>
-//       </div>`;
-
-//       if (e.target.innerHTML === '상세') {
-//         e.target.parentNode.parentNode.insertAdjacentHTML(
-//           'afterend',
-//           detailHtml
-//         );
-//         e.target.innerHTML = '닫기';
-//       } else {
-//         e.target.innerHTML = '상세';
-//         $(`#cls${_id}`).remove();
-//       }
-//     });
-//   });
-// }
-
-export { showCategories };
+export { initFunc as showCategories };
