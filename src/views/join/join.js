ff66267f863c9d466fa8c3d5f9b8652a5e95de8e
sojinjang <jsojin6119@gmail.com>
2022-11-09 20:08:12 +0900

    :bug: debug(join): modify email and name check logic

--- src/views/join/join.js
+++ src/views/join/join.js
@@ -1,82 +1,63 @@
-import footer from "../template/footer/footer.js";
-//import * as Api from "/api.js";
-
 const body = document.querySelector(".body-container");
 const nameInput = document.querySelector("#nameInput");
 const emailInput = document.querySelector("#emailInput");
 const joinBtn = document.querySelector(".join-form-button");
-const overlabBtn = document.querySelector(".emailOverlap");
+const checkOverlabBtn = document.querySelector(".emailOverlap");
 
-// const successUserInfo = [];
-// function pushUserInfo() {
-//     let email = emailInput.value;
-//     let name = emailInput.value;
-//     successUserInfo.push(email);
-//     successUserInfo.push(name);
-// }
-// pushUserInfo();
+let emailCleard = false;
 
-
-body.insertAdjacentHTML("afterend", footer());
-
-joinBtn.addEventListener("click", joinCheck)
-
-overlabBtn.addEventListener("click", checkOverlab)
-
-let tempStatus = true;
-
-function checkOverlab() {
-
-    const emailinput = emailInput.value;
-    const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
-    function 중복체크(emailinput) {
-        const data = ["elice@test.com"]; // 임시 데이터
-    
-        if(data.includes(emailinput) == true || emailinput == "") {
-            return true;
-        }
-        return false;
-    }
-    
-    if(emailinput == "") {
-        alert("이메일을 입력하세요.")
-        return;
-    }
-    if(중복체크(emailinput)) {
-        alert("이미 사용중인 이메일입니다.")
-        tempStatus = false;
-        return;
-    }
-    if(emailinput.match(emailRegExp) == null) {
-        alert("이메일 형식을 다시 확인해주세요.")
-        tempStatus = false;
-        return;
-    }
-    if(!중복체크(emailinput)) {
-        alert("사용 가능합니다!")
-        tempStatus = true;
-        return;
-    }
-
+function isName(name) {
+  return /^[가-힣]{2,4}/.test(name);
 }
 
-function joinCheck(e) {
-    e.preventDefault();
-    
-    if(nameInput.value.length <= 1) {
-        alert("이름은 두 글자 이상 입력해야 합니다.")
-        return;
-    }
-    if(!tempStatus || nameInput.value == "" ) {
-        alert("입력창을 다시 확인하세요.");
-        return;
-    }
-    if(tempStatus){
-        const successUserInfo = [nameInput.value, emailInput.value]
-        location.href = `join-success.html?${successUserInfo}`;
-    }
+function isValidEmail(email) {
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  return emailRegExp.test(email);
 }
 
+function isDuplicatedEmail(email) {
+  const data = ["elice@test.com"]; // 임시 데이터
 
+  if (data.includes(email)) {
+    return true;
+  }
+  return false;
+}
 
+function checkEmail() {
+  const email = emailInput.value;
+
+  if (!isValidEmail(email)) {
+    alert("유효한 이메일 형식이 아닙니다.");
+    emailCleard = false;
+    return;
+  }
+  if (isDuplicatedEmail(email)) {
+    alert("이미 사용중인 이메일입니다.");
+    emailCleard = false;
+    return;
+  }
+  emailCleard = true;
+}
+
+function moveToNextPage() {
+  location.href = `join-success.html?${[nameInput.value, emailInput.value]}`;
+}
+
+function checkRequest(e) {
+  e.preventDefault();
+  if (!isName(nameInput.value)) {
+    alert("이름 입력값을 확인해주세요.");
+    return;
+  }
+  if (!emailCleard) {
+    alert("이메일 중복검사를 실행해주세요.");
+    return;
+  } else {
+    moveToNextPage();
+  }
+}
+
+checkOverlabBtn.addEventListener("click", checkEmail);
+joinBtn.addEventListener("click", checkRequest);
