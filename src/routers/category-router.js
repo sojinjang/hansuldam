0b6afe5aad1e94502455dac367a78330c3873760
dejaikeem <dejaikeem@gmail.com>
2022-11-10 02:34:00 +0900

    :sparkles: feat(pagination): add pagination at productList
    category-router get요청에 req.params에 설정한 카테고리에 해당하는 상품 리스트의 pagination 기능 구현

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -40,12 +40,34 @@ categoryRouter.get("/:categoryId/products", async (req, res, next) => {
     if (!categoryId) {
       throw new BadRequest("Undefined params", 4005);
     }
-    const { products } = await categoryService.getCategoryById(categoryId);
+    const page = Number(req.query.page || 1);
+    const perPage = Number(req.query.perPage || 9);
 
-    const productList = await productService.getProductList(products);
+    const { products } = await categoryService.getCategoryById(categoryId);
+    let productList = await productService.getProductList(products);
+    // 페이지네이션
+    let arr = [];
+    for (let i = 0; i < productList.length; i++) {
+      arr.push(productList[i]);
+    }
+
+    const productsPerPage = arr.slice(
+      perPage * (page - 1),
+      perPage * (page - 1) + perPage
+    );
+
+    const total = arr.length;
+    const totalPage = Math.ceil(total / perPage);
+    productList = productsPerPage;
+
+    const result = {
+      totalPage,
+      total,
+      productList,
+    };
 
     // 주문 목록(배열)을 JSON 형태로 프론트에 보냄
-    res.status(200).json(productList);
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
