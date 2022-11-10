636a0a984cfb66a60b9811230791a46b202a131b
sojinjang <jsojin6119@gmail.com>
2022-11-10 19:26:14 +0900

    :sparkles: feat(pay): debug some error and add api request

--- src/views/order_page/order_page.js
+++ src/views/order_page/order_page.js
@@ -1,11 +1,13 @@
+import * as api from "../api.js";
+import { isCardNum } from "../utils/validator.js";
+
 const $ = (seletor) => document.querySelector(seletor);
 
 const PRODUCTS_KEY = "products";
+
 function getSavedProducts() {
   return JSON.parse(localStorage.getItem(PRODUCTS_KEY));
 }
-let savedProducts = getSavedProducts();
-console.log(savedProducts);
 
 function showProduct(item) {
   let product = undefined;
@@ -23,21 +25,17 @@ function showProduct(item) {
 </div>
 <div class="product-info">
     <p class="product-quantity">${parseInt(item.quantity)}개</p>
-    <p class="product-price">${(item.price * item.quantity).toLocaleString(
-      "ko-KR"
-    )}원</p>
+    <p class="product-price">${(item.price * item.quantity).toLocaleString("ko-KR")}원</p>
 </div>`;
   $(".add-product").append(product);
 }
-
-savedProducts.forEach(showProduct);
 
 function getPureDigit(numStr) {
   const regex = /[^0-9]/g;
   return parseInt(numStr.replace(regex, ""));
 }
 
-function getListsPrice() {
+function getAllProductsPrice() {
   const priceSelect = document.querySelectorAll(".product-price");
   let totalPrice = 0;
   let priceArr = [];
@@ -47,59 +45,27 @@ function getListsPrice() {
   priceArr.forEach((v) => {
     totalPrice += getPureDigit(v);
   });
-  $(".total-product-price").innerText = `${totalPrice.toLocaleString(
-    "ko-KR"
-  )}원`;
+  $(".total-product-price").innerText = `${totalPrice.toLocaleString("ko-KR")}원`;
   return totalPrice;
 }
 
-function getDeliveryFee() {
-  let deliveryPay = 0;
-  getListsPrice() >= 50000 ? (deliveryPay = 0) : (deliveryPay = 3000);
-  $(".delivery-fee").innerText = `${deliveryPay.toLocaleString("ko-KR")}원`;
-  return deliveryPay;
+function getDeliveryFee(price) {
+  const deliveryFee = price < 50000 ? (price > 0 ? 3000 : 0) : 0;
+  $(".delivery-fee").innerText = `${deliveryFee.toLocaleString("ko-KR")}원`;
+  return deliveryFee;
 }
 
 function caculateTotalPrice() {
-  const productsPrice = getListsPrice();
-  const deliveryFee = getDeliveryFee();
-  $(".total-payment-price").innerText = `${(
-    productsPrice + deliveryFee
-  ).toLocaleString("ko-KR")}원`;
+  const productsPrice = getAllProductsPrice();
+  const deliveryFee = getDeliveryFee(productsPrice);
+  $(".total-payment-price").innerText = `${(productsPrice + deliveryFee).toLocaleString(
+    "ko-KR"
+  )}원`;
   return productsPrice + deliveryFee;
 }
 
-caculateTotalPrice();
-
-$(".creditCardBtn").addEventListener("click", function (e) {
-  e.preventDefault();
-  $(".creditCard").style.display = "block";
-});
-
-// 임시 사용자 데이터
-const userData = {
-  fullName: "김뫄뫄",
-  phoneNumber: "01012341234",
-  email: "elice.test.com",
-  address: {
-    postalCode: "44123",
-    address1: "ㅇㅇ시 ㅇㅇ구 ㅇㅇ동",
-    address2: "ㅇㅇ아파트 101동 101호",
-  },
-};
-
-// 주문자, 배송정보
-$(".user-name").innerHTML = userData.fullName;
-$(".user-phoneNumber").innerHTML = userData.phoneNumber;
-$(".user-PostalCode").innerHTML = userData.address.postalCode;
-$(".user-address1").innerHTML = userData.address.address1;
-$(".user-address2").innerHTML = userData.address.address2;
-
-$("#select-status").addEventListener("change", showInput);
-
 function showInput(e) {
   const type = e.target.value;
-
   if (type === "direct") {
     $("#input-hide").style.display = "flex";
   } else {
@@ -107,59 +73,105 @@ function showInput(e) {
   }
 }
 
-const priceSum = caculateTotalPrice();
-
-const res = JSON.parse(localStorage.getItem("products"));
-
-const productsInOrder = res.map((item) => ({
-  _id: item._id,
-  quantity: item.quantity,
-}));
-
-// 유저정보 api요청하기
-
-// db에 담을 데이터
-const orderData = {
-  fullName: userData.fullName,
-  phoneNumber: userData.phoneNumber,
-  address: {
-    postalCode: userData.address.postalCode,
-    address1: userData.address.address1,
-    address2: userData.address.address2,
-  },
-  status: "상품준비중",
-  priceSum: priceSum,
-  productsInOrder: productsInOrder,
-};
-
-// 결제 버튼 눌렀을 때
-const paymentBtn = $("#rounded-button");
-
-paymentBtn.addEventListener("click", payOrder);
-
-function payOrder(e) {
-  e.preventDefault();
-  const select = $("#select-status").value;
-  if ($(".add-product").children.length == 0) {
-    alert("주문 목록이 없습니다!");
-    return;
-  }
-  if (select === "0") {
-    alert("배송 시 요청사항을 선택해주세요!");
-    return;
-  }
-  // 카드입력 내용 조건 걸기
-  if (
-    $("#input1").innerHTML == "" &&
-    $("#input2").innerHTML == "" &&
-    $("#input3").innerHTML == "" &&
-    $("#input4").innerHTML == ""
-  ) {
-    alert("카드 입력칸을 다시 확인해주세요!");
-    return;
-  }
-  console.log("hi");
-  // 주문내용들 api로 post 보내기
-  // 주문 완료 페이지로 이동하기
-  //window.location.href()
+function writeUserInfo(userInfoObj) {
+  $(".user-name").value = userInfoObj.fullName;
+  $(".user-phoneNumber").value = userInfoObj.phoneNumber;
+  $(".user-address1").value = userInfoObj.address.address1;
+  $(".user-address2").value = userInfoObj.address.address2;
 }
+
+async function getUserInfo() {
+  try {
+    const userDataObj = await api.get("/api/auth/user");
+    return userDataObj;
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+function showCardInfoForm(e) {
+  e.preventDefault();
+  $(".creditCard").style.display = "block";
+}
+
+function checkPayInfo() {
+  if ($(".add-product").children.length === 0) {
+    alert("주문 목록이 없습니다 🤔");
+    return;
+  }
+  if ($("#card-select").value === "0") {
+    alert("카드사를 선택해주세요 💳");
+    return;
+  }
+  if (
+    !isCardNum($(".creditCardInput1").value) ||
+    !isCardNum($(".creditCardInput2").value) ||
+    !isCardNum($(".creditCardInput3").value) ||
+    !isCardNum($(".creditCardInput4").value)
+  ) {
+    alert("카드 번호 입력칸을 확인해주세요 🔢");
+    return;
+  }
+  return true;
+}
+
+function makeProductsInOrder(items) {
+  console.log(savedProducts);
+  const productsArr = items.map((item) => ({
+    id: item._id,
+    quantity: item.quantity,
+  }));
+  console.log(productsArr);
+  return productsArr;
+}
+
+function makeOrderInfoObj() {
+  return {
+    fullName: $(".user-name").value,
+    phoneNumber: $(".user-phoneNumber").value,
+    address: {
+      address1: $(".user-address1").value,
+      address2: $(".user-address2").value,
+    },
+    payment: {
+      method: "신용카드",
+      detail: "현대",
+      number:
+        $(".creditCardInput1").value +
+        $(".creditCardInput2").value +
+        $(".creditCardInput3").value +
+        $(".creditCardInput4").value,
+    },
+    priceSum: getPureDigit($(".total-payment-price").innerText),
+    productsInOrder: makeProductsInOrder(savedProducts),
+  };
+}
+
+async function requestPostOrder(orderInfoObj) {
+  try {
+    await api.post("/api/auth/orders", orderInfoObj);
+    window.location.href = "/order-completed";
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+function sendPayInfo() {
+  if (checkPayInfo()) {
+    const orderInfoObj = makeOrderInfoObj();
+    requestPostOrder(orderInfoObj);
+  }
+}
+
+let savedProducts = getSavedProducts();
+savedProducts.forEach(showProduct);
+caculateTotalPrice();
+
+const userInfoObj = await getUserInfo();
+writeUserInfo(userInfoObj);
+
+$("#delivery-select").addEventListener("change", showInput);
+$("#card-select").addEventListener("change", showInput);
+$(".creditCardBtn").addEventListener("click", showCardInfoForm);
+
+$(".pay-button").addEventListener("click", sendPayInfo);
