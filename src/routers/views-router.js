ae24b92d4fe8862fc02a4a584c86ab323185d2fd
sojinjang <jsojin6119@gmail.com>
2022-11-10 23:24:04 +0900

    :sparkles: feat(pay_nonmember): add a page for checking adult identification when non-member tries to purchase

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -17,6 +17,7 @@ viewsRouter.use("/event-page", serveStatic("event_page"));
 viewsRouter.use("/admin", serveStatic("admin"));
 viewsRouter.use("/order-pay-member", serveStatic("order_pay_member"));
 viewsRouter.use("/order-pay-nonmember", serveStatic("order_pay_nonmember"));
+viewsRouter.use("/adult-certification", serveStatic("adult_certification"));
 viewsRouter.use("/order-list", serveStatic("order_list"));
 viewsRouter.use("/find-password", serveStatic("find_password"));
 
