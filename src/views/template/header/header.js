25edf570a0f0a3db23af9034b030ddfc3e8dfb6d
wooooooongs <pires11233@gmail.com>
2022-11-10 22:31:20 +0900

    :sparkles: feat: add function for delete cookie
    쿠키의 키와 값을 삭제하는 함수를 추가하였습니다.

--- src/views/template/header/header.js
+++ src/views/template/header/header.js
@@ -1,3 +1,5 @@
+import { deleteCookie, getCookieValue, setCookie } from '../../utils/cookie.js';
+
 function getHeader() {
   return `<header> 
       <div class="header-container">
@@ -40,22 +42,45 @@ function getHeader() {
 }
 
 function redirectPage() {
-  const $ = selector => document.querySelector(selector);
+  const $ = (selector) => document.querySelector(selector);
   const menuLabels = document.querySelectorAll('.menu-label');
+  const TOKEN = 'token';
 
-  $(".company-logo").addEventListener("click", () => (window.location.href = "/"));
-  $(".login").addEventListener("click", () => (window.location.href = "/login"));
-  $('.join').addEventListener("click", () => (window.location.href = "/join"));
-  $('.basket').addEventListener("click", () => (window.location.href = "/cart"));
-  $('#eventProducts').addEventListener('click', () => (window.location.href = '/event-page'));
-  
+  $('.company-logo').addEventListener(
+    'click',
+    () => (window.location.href = '/')
+  );
+  $('.join').addEventListener('click', () => (window.location.href = '/join'));
+  $('.login').addEventListener('click', () => {
+    window.location.href = '/login';
+  });
+
+  if (getCookieValue(TOKEN)) {
+    $('.user-list').innerHTML = `<li class="logout">로그아웃</li>
+<div class="basket">
+  <img src="../img/shopping-bag.png" alt="cart-img">
+</div>`;
+    $('.logout').addEventListener('click', () => {
+      deleteCookie(TOKEN);
+      window.location.reload();
+    });
+  }
+
+  $('.basket').addEventListener(
+    'click',
+    () => (window.location.href = '/cart')
+  );
+  $('#eventProducts').addEventListener(
+    'click',
+    () => (window.location.href = '/event-page')
+  );
+
   menuLabels.forEach((label) => {
     label.addEventListener('click', (e) => {
       const labelId = e.currentTarget.getAttribute('id');
       window.location.href = `/products?label=${labelId}`;
     });
   });
-  
 }
 
 export { getHeader, redirectPage };
