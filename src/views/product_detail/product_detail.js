aad74a74acf0f91191e310eb074c094b5e11de2c
wooooooongs <pires11233@gmail.com>
2022-11-16 20:26:08 +0900

    :bug: fix: fix a bug when localStorage is empty
    로컬스토리지 내 products키가 null일 때, 장바구니에 추가되지 않는 버그를 수정하였습니다. 또한 prettier적용을 하였습니다.

--- src/views/product_detail/product_detail.js
+++ src/views/product_detail/product_detail.js
@@ -1,24 +1,22 @@
-import { get } from "../api.js";
-import { changeToKoreanTime } from "../utils/useful_functions.js";
-import { getCookieValue } from "../utils/cookie.js";
+import { get } from '../api.js';
+import { changeToKoreanTime } from '../utils/useful_functions.js';
+import { getCookieValue } from '../utils/cookie.js';
 
-const PRODUCTS_KEY = "products";
+const PRODUCTS_KEY = 'products';
 
 async function renderData() {
   const queryString = new Proxy(new URLSearchParams(window.location.search), {
     get: (params, prop) => params.get(prop),
   });
   const currentId = queryString.id;
-  const fetchedData = await get("/api/products", currentId);
+  const fetchedData = await get('/api/products', currentId);
   const {
     _id,
     category,
-    brand,
     name,
     price,
     volume,
     description,
-    sales,
     alcoholType,
     alcoholDegree,
     manufacturedDate,
@@ -26,10 +24,10 @@ async function renderData() {
 
   document.title = `${name} - 한술담 🍶`;
 
-  let productSection = document.createElement("section");
+  let productSection = document.createElement('section');
 
-  productSection.setAttribute("class", "product-container");
-  productSection.setAttribute("id", _id);
+  productSection.setAttribute('class', 'product-container');
+  productSection.setAttribute('id', _id);
   productSection.innerHTML = `<div class="product-container">
   <div class="image-warpper">
     <img src="../img/ricewine_icon.png" alt="상품 이미지" />
@@ -38,7 +36,9 @@ async function renderData() {
 		<div class="content__main-info">
     <p class="content__item content__name">${name}</p>
     <p class="content__item content__category">${category}</p>
-			<p class="content__item content__price">${Number(price).toLocaleString("ko-KR")}원</p>
+			<p class="content__item content__price">${Number(price).toLocaleString(
+        'ko-KR'
+      )}원</p>
 			<p class="content__desc">${description}</p>
 		</div>
 		<div class="content__detail-info">
@@ -73,7 +73,7 @@ async function renderData() {
 	</div>
 </div>`;
 
-  const bodyContainer = document.querySelector(".body-container");
+  const bodyContainer = document.querySelector('.body-container');
 
   bodyContainer.append(productSection);
 
@@ -83,46 +83,55 @@ async function renderData() {
 async function orderAndCart() {
   let productData = await renderData();
 
-  const orderButton = document.querySelector("#order-button");
-  const basketButton = document.querySelector("#basket-button");
+  const orderButton = document.querySelector('#order-button');
+  const basketButton = document.querySelector('#basket-button');
 
-  orderButton.addEventListener("click", clickOrder);
-  basketButton.addEventListener("click", clickCart);
+  orderButton.addEventListener('click', clickOrder);
+  basketButton.addEventListener('click', clickCart);
 
   function clickOrder() {
-    const TOKEN = "token";
-    const PRODUCTS_KEY = "products";
+    const TOKEN = 'token';
+    const PRODUCTS_KEY = 'products';
     productData.quantity = 1;
 
     let tempArr = [productData];
 
     localStorage.setItem(PRODUCTS_KEY, JSON.stringify(tempArr));
 
-    if (getCookieValue(TOKEN) === undefined || getCookieValue(TOKEN) == "") {
-      window.location.href = "/order-pay-nonmember";
+    if (getCookieValue(TOKEN) === undefined || getCookieValue(TOKEN) == '') {
+      window.location.href = '/order-pay-nonmember';
     } else {
-      window.location.href = "/order-pay-member";
+      window.location.href = '/order-pay-member';
     }
   }
 
   function clickCart() {
-    productData["quantity"] = 1;
-    let cartItems = JSON.parse(localStorage.getItem(PRODUCTS_KEY));
-    const existItemIdx = cartItems.findIndex(
-      (product) => product._id === productData._id
-    );
-    if (existItemIdx === -1) {
-      cartItems = [...cartItems, productData];
+    productData['quantity'] = 1;
+
+    if (!JSON.parse(localStorage.getItem(PRODUCTS_KEY))) {
+      // 로컬스토리지 내 PRODUCTS_KEY값이 비어있을 때, null
+      let productArray = [productData];
+      localStorage.setItem(PRODUCTS_KEY, JSON.stringify(productArray));
     } else {
-      cartItems[existItemIdx].quantity += 1;
+      let cartItems = JSON.parse(localStorage.getItem(PRODUCTS_KEY));
+      const existItemIdx = cartItems.findIndex(
+        (product) => product._id === productData._id
+      );
+
+      if (existItemIdx === -1) {
+        // 로컬스토리지 내 PRODUCTS_KET값은 존재하나 비어있을 때, undefined
+        cartItems = [...cartItems, productData];
+      } else {
+        cartItems[existItemIdx].quantity += 1;
+      }
+      localStorage.setItem(PRODUCTS_KEY, JSON.stringify(cartItems));
     }
-    localStorage.setItem(PRODUCTS_KEY, JSON.stringify(cartItems));
 
     // Message
-    const cartMessage = document.querySelector(".cart-message");
-    cartMessage.classList.add("fade-message");
+    const cartMessage = document.querySelector('.cart-message');
+    cartMessage.classList.add('fade-message');
     setTimeout(() => {
-      cartMessage.classList.remove("fade-message");
+      cartMessage.classList.remove('fade-message');
     }, 1000);
   }
 }
