575a804b057277469c41a0f4c4732e3b965a1400
wooooooongs <pires11233@gmail.com>
2022-11-20 21:48:00 +0900

    :bug: fix: fix product detail button and simplify code
    상품 상세보기 버튼을 누르고 또 다른 상세 버튼을 눌렀을 때, 여전히 상세라고 뜨는 버그를 고쳤습니다.

--- src/views/admin/product_admin.js
+++ src/views/admin/product_admin.js
@@ -40,7 +40,7 @@ async function openProductMenu() {
   <div class="column is-2 row-name">이름/브랜드</div>
   <div class="column is-2 row-price">가격/재고</div>
   <div class="column is-2 row-volumn">용량/설명</div>
-  <div class="column is-2 row-category">카테고리/판매수</div>
+  <div class="column is-2 row-category">카테고리/판매량</div>
   <div class="column is-2 row-type">타입/제조일자</div>
   <div class="column is-1 row-type">상세/도수</div>
   <div class="column is-1 row-type">삭제</div>
@@ -77,33 +77,36 @@ function addProduct() {
   if ($(".modify-product-modal")) {
     $(".modify-product-modal").remove();
   }
-
-  if ($(".modify-product-modal")) {
-  }
   $(".add-button").classList.add("none");
   $(".close-button").classList.add("none");
 
-  const productModalHtml = `<label class="add-product-modal">
-  <div class="left-modal">
-    <input id="name" class="input is-rounded product-input" type="text" placeholder="이름" />
-    <input id="price" class="input is-rounded product-input" type="text" placeholder="가격" />
-    <input id="volume" class="input is-rounded product-input" type="text" placeholder="용량(ml)" />
-    <input id="category" class="input is-rounded product-input" type="text" placeholder="카테고리" />
-    <input id="brand" class="input is-rounded product-input" type="text" placeholder="브랜드명" />
+  const productModalHtml = `<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <label class="add-product-modal">
+      <div class="left-modal">
+        <input id="name" class="input is-rounded product-input" type="text" placeholder="이름" />
+        <input id="price" class="input is-rounded product-input" type="text" placeholder="가격" />
+        <input id="volume" class="input is-rounded product-input" type="text" placeholder="용량(ml)" />
+        <input id="category" class="input is-rounded product-input" type="text" placeholder="카테고리" />
+        <input id="alcoholType" class="input is-rounded product-input" type="text" placeholder="종류(탁주)" />
+      </div>
+      <div class="right-modal">
+        <input id="brand" class="input is-rounded product-input" type="text" placeholder="브랜드명" />
+        <input id="stock" class="input is-rounded product-input" type="text" placeholder="재고" />
+        <input id="description" class="input is-rounded product-input" type="text" placeholder="설명" />
+        <input id="sales" class="input is-rounded product-input" type="text" placeholder="판매량" />
+        <input id="manufacturedDate" class="input is-rounded product-input" type="text" placeholder="제조일자" />
+        <input id="alcoholDegree" class="input is-rounded product-input" type="text" placeholder="도수" />
+        <div>
+          <button class="button add-product-button">추가</button>
+          <button class="button close-modal-button">닫기</button>
+        </div>
+      </div>
+    </label>
   </div>
-  <div class="right-modal">
-    <input id="description" class="input is-rounded product-input" type="text" placeholder="설명" />
-    <input id="stock" class="input is-rounded product-input" type="text" placeholder="재고" />
-    <input id="sales" class="input is-rounded product-input" type="text" placeholder="판매량" />
-    <input id="alcoholType" class="input is-rounded product-input" type="text" placeholder="종류(탁주)" />
-    <input id="alcoholDegree" class="input is-rounded product-input" type="text" placeholder="도수" />
-  <div>
-    <button class="button add-product-button">추가</button>
-    <button class="button close-modal-button">닫기</button>
-  </div>
-  </div>
-</label>
-</form>`;
+  <button class="modal-close is-large"></button>
+</div>`;
 
   $(".admin-menu").insertAdjacentHTML("afterend", productModalHtml);
 
@@ -157,13 +160,11 @@ async function renderProduct(product) {
   productSection.setAttribute("class", "columns items-container");
   productSection.setAttribute("id", _id);
   productSection.innerHTML = `<div class="column is-2 row-name">${name}</div>
-<div class="column is-2 row-price">${Number(price).toLocaleString(
-    "ko-KR"
-  )}원</div>
+<div class="column is-2 row-price">${Number(price).toLocaleString("ko-KR")}원</div>
 <div class="column is-2 row-volumn">${volume}ml</div>
 <div class="column is-2 row-category">${category}</div>
 <div class="column is-2 row-type">${alcoholType}</div>
-<div class="column is-1"><button id="${_id}" class="button column detail-button">상세</button></div>
+<div class="column is-1"><button id="btn-${_id}" class="button column detail-button">상세</button></div>
 <div class="column is-1"><button id="${_id}" class="button column delete-button">삭제</button></div>
 `;
 
@@ -189,20 +190,11 @@ async function renderProductDetail() {
 
   detailBtn.forEach((button) => {
     button.addEventListener("click", (e) => {
-      const currentId = e.target.getAttribute("id");
-      const currentIndex = productsData.findIndex(
-        (product) => product._id === currentId
-      );
+      const currentId = e.target.getAttribute("id").split("-")[1];
+      const currentIndex = productsData.findIndex((product) => product._id === currentId);
       const currentData = productsData[currentIndex];
-      const {
-        _id,
-        stock,
-        brand,
-        description,
-        sales,
-        manufacturedDate,
-        alcoholDegree,
-      } = currentData;
+      const { _id, stock, brand, description, sales, manufacturedDate, alcoholDegree } =
+        currentData;
 
       const detailHtml = `<div class="columns items-container items-detail opened" id="${_id}">
       <div class="column is-2">${brand}</div>
@@ -218,16 +210,17 @@ async function renderProductDetail() {
 
       if (e.target.innerHTML === "상세") {
         if ($(".items-detail")) {
-          $(".opened").remove();
+          const detailButtons = document.querySelectorAll(".detail-button");
+          detailButtons.forEach((button) => {
+            button.innerHTML = "상세";
+          });
+          $(".items-detail").remove();
         }
-        e.target.parentNode.parentNode.insertAdjacentHTML(
-          "afterend",
-          detailHtml
-        );
+        e.target.parentNode.parentNode.insertAdjacentHTML("afterend", detailHtml);
         e.target.innerHTML = "닫기";
       } else {
         e.target.innerHTML = "상세";
-        $(".opened").remove();
+        $(".items-detail").remove();
       }
 
       const modifyBtn = document.querySelector(".modify-button");
@@ -236,7 +229,7 @@ async function renderProductDetail() {
           if ($(".add-product-modal")) {
             $(".add-product-modal").remove();
           }
-          modifyProduct();
+          modifyProduct(currentData);
           window.scrollTo(0, 580);
         });
       }
@@ -253,10 +246,7 @@ function deleteProduct() {
         alert("삭제 하려면 다시 한 번 눌러주세요!");
       }
 
-      e.target.setAttribute(
-        "class",
-        "button column is-danger delete-button-confirm"
-      );
+      e.target.setAttribute("class", "button column is-danger delete-button-confirm");
 
       $(".delete-button-confirm").addEventListener("click", async () => {
         await del(ApiUrl.ADMIN_PRODUCTS, currentId);
@@ -266,31 +256,34 @@ function deleteProduct() {
   });
 }
 
-function modifyProduct() {
+function modifyProduct(currentData) {
+  const {} = currentData;
+
   if ($(".modify-product-modal")) {
     $(".modify-product-modal").remove();
   }
   const productId = $(".opened").getAttribute("id");
   const productModalHtml = `<form>
 <label class="modify-product-modal">
-<div class="left-modal">
-  <input id="name" class="input is-rounded product-input" type="text" placeholder="이름" />
-  <input id="price" class="input is-rounded product-input" type="text" placeholder="가격" />
-  <input id="volume" class="input is-rounded product-input" type="text" placeholder="용량(ml)" />
-  <input id="category" class="input is-rounded product-input" type="text" placeholder="카테고리" />
-  <input id="brand" class="input is-rounded product-input" type="text" placeholder="브랜드명" />
-</div>
-<div class="right-modal">
-  <input id="description" class="input is-rounded product-input" type="text" placeholder="설명" />
-  <input id="stock" class="input is-rounded product-input" type="text" placeholder="재고" />
-  <input id="sales" class="input is-rounded product-input" type="text" placeholder="판매량" />
-  <input id="alcoholType" class="input is-rounded product-input" type="text" placeholder="종류(탁주)" />
-  <input id="alcoholDegree" class="input is-rounded product-input" type="text" placeholder="도수" />
-  <div>
-    <button class="button modify-product-button">수정</button>
-    <button class="button close-modal-button">닫기</button>
+  <div class="left-modal">
+    <input id="name" class="input is-rounded product-input" type="text" placeholder="이름" />
+    <input id="price" class="input is-rounded product-input" type="text" placeholder="가격" />
+    <input id="volume" class="input is-rounded product-input" type="text" placeholder="용량(ml)" />
+    <input id="category" class="input is-rounded product-input" type="text" placeholder="카테고리" />
+    <input id="alcoholType" class="input is-rounded product-input" type="text" placeholder="종류(탁주)" />
   </div>
-</div>
+  <div class="right-modal">
+    <input id="brand" class="input is-rounded product-input" type="text" placeholder="브랜드명" />
+    <input id="stock" class="input is-rounded product-input" type="text" placeholder="재고" />
+    <input id="description" class="input is-rounded product-input" type="text" placeholder="설명" />
+    <input id="sales" class="input is-rounded product-input" type="text" placeholder="판매량" />
+    <input id="manufacturedDate" class="input is-rounded product-input" type="text" placeholder="제조일자" />
+    <input id="alcoholDegree" class="input is-rounded product-input" type="text" placeholder="도수" />
+    <div>
+      <button class="button modify-product-button">수정</button>
+      <button class="button close-modal-button">닫기</button>
+    </div>
+  </div>
 </label>
 </form>`;
 
