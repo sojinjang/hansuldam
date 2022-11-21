c54dc481e2845132ab6c4345bdfc270445e46b5d
wooooooongs <pires11233@gmail.com>
2022-11-21 12:49:42 +0900

    :zap: refactor: update fetch data functions
    코드의 최적화를 통해 불필요한 요청을 줄였으며, 카테고리 페이지 내 모든 상품이 보이지 않는 버그를 고쳤습니다.

--- src/views/admin/category_admin.js
+++ src/views/admin/category_admin.js
@@ -11,8 +11,23 @@ function initFunc() {
   });
 }
 
+async function fetchProducts(index) {
+  const data = await get(`${ApiUrl.PRODUCTS_OVERALL_INFORMATION}${index}&perpage=9`);
+
+  return data;
+}
+
 async function openCategoryMenu() {
   const categoriesData = await get(ApiUrl.CATEGORY);
+  const { products, totalPage } = await fetchProducts(1);
+  let productsTotalData = products;
+
+  for (let i = 2; i <= totalPage; i++) {
+    (await fetchProducts(i))["products"].forEach((product) => {
+      productsTotalData.push(product);
+    });
+  }
+
   $(".category-menu").classList.add("isClicked");
 
   const productContainerHTML = `<section class="categories-container">
@@ -26,7 +41,7 @@ async function openCategoryMenu() {
 
   await $(".admin-menu").insertAdjacentHTML("afterend", productContainerHTML);
   categoriesData.forEach(async (category, index) => {
-    await renderCategory(category);
+    await renderCategory(category, productsTotalData);
     if (index == categoriesData.length - 1) {
       modifyCategory();
       deleteCategory();
@@ -76,26 +91,9 @@ async function postCategory(inputValue) {
   await post("/api/admin/category", inputValueObject);
 }
 
-async function fetchProducts(index) {
-  const data = await get(`${ApiUrl.PRODUCTS_OVERALL_INFORMATION}${index}`)
-
-  return data;
-}
-
-async function renderCategory(category) {
+async function renderCategory(category, productsTotalData) {
   const { _id, name } = await category;
-  const fetchProductsData = await get(ApiUrl.PRODUCTS);
-  const pageOneProducts = fetchProductsData["products"];
   let { products } = await category;
-  let productsTotalData = pageOneProducts;
-
-  let totalPage = fetchProductsData["totalPage"];
-
-  for (let i = 2; i <= totalPage; i++) {
-    (await fetchProducts(i))["products"].forEach((product) => {
-      productsTotalData.push(product);
-    });
-  }
 
   const productsArr = products.reduce((arr, productId) => {
     const currentProductIndex = productsTotalData.findIndex(
