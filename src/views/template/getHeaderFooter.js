3713c1f2fa456976b743a4cb83eea3623e780f16
sojinjang <jsojin6119@gmail.com>
2022-11-24 22:45:48 +0900

    :sparkles: feat(oauth): resend request by userId for naver login

--- src/views/template/getHeaderFooter.js
+++ src/views/template/getHeaderFooter.js
@@ -3,6 +3,8 @@ import { getHeader, redirectPage } from "./header/header.js";
 import { setCookie } from "../utils/cookie.js";
 import { Keys } from "../constants/Keys.js";
 import { getCartInfoFromDB } from "../utils/cart.js";
+import * as api from "../api.js";
+import { ApiUrl } from "../constants/ApiUrl.js";
 
 const main = document.querySelector(".body-container");
 
@@ -10,10 +12,20 @@ const currentUrl = window.location.href;
 
 main.insertAdjacentHTML("beforebegin", getHeader());
 main.insertAdjacentHTML("afterend", getFooter());
-if (currentUrl.includes(Keys.NAVER_TOKEN_KEY)) {
-  const token = currentUrl.split(Keys.NAVER_TOKEN_KEY)[1].slice(1);
-  setCookie(Keys.TOKEN_KEY, { [Keys.TOKEN_KEY]: token });
-  getCartInfoFromDB();
+
+async function logInByLogin(userId) {
+  try {
+    const response = await api.get(ApiUrl.NAVER_OAUTH, userId);
+    setCookie(Keys.TOKEN_KEY, { [Keys.TOKEN_KEY]: response.token });
+    setCookie(Keys.USER_ID_KEY, { [Keys.USER_ID_KEY]: response.userId });
+    getCartInfoFromDB();
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+if (currentUrl.includes(Keys.NAVER_KEY)) {
+  await logInByLogin(currentUrl.split(Keys.NAVER_KEY)[1].slice(1));
 }
 
 redirectPage();
