0a2a67508c42b12fb46da6e07ba00ea117abaaf0
wooooooongs <pires11233@gmail.com>
2022-11-25 14:49:03 +0900

    :lipstick: design: 검색창과 main css를 적용하였습니다.

--- src/views/template/header/header.js
+++ src/views/template/header/header.js
@@ -15,8 +15,31 @@ function getHeader() {
             <img class="company-logo" src="../img/logo.png" alt="company-logo">
           </div>
           <div class="search-container">
-            <img class="search-img" src="../img/search-icon.png" alt="search-logo">
-            <input class="input is-rounded search-bar" type="text" placeholder="제품검색" />
+            <svg xmlns="http://www.w3.org/2000/svg" style="display:none">
+              <symbol xmlns="http://www.w3.org/2000/svg" id="sbx-icon-search-13" viewBox="0 0 40 40">
+                <path d="M25.54 28.188c-2.686 2.115-6.075 3.376-9.758 3.376C7.066 31.564 0 24.498 0 15.782 0 7.066 7.066 0 15.782 0c8.716 0 15.782 7.066 15.782 15.782 0 4.22-1.656 8.052-4.353 10.884l1.752 1.75 1.06-1.06L40 37.332l-3.72 3.72-9.977-9.976 1.062-1.062-1.826-1.826zm-9.758.746c7.264 0 13.152-5.888 13.152-13.152 0-7.263-5.888-13.152-13.152-13.152C8.52 2.63 2.63 8.52 2.63 15.782c0 7.264 5.89 13.152 13.152 13.152z"
+                fill-rule="evenodd" />
+              </symbol>
+              <symbol xmlns="http://www.w3.org/2000/svg" id="sbx-icon-clear-2" viewBox="0 0 20 20">
+                <path d="M8.96 10L.52 1.562 0 1.042 1.04 0l.522.52L10 8.96 18.438.52l.52-.52L20 1.04l-.52.522L11.04 10l8.44 8.438.52.52L18.96 20l-.522-.52L10 11.04l-8.438 8.44-.52.52L0 18.96l.52-.522L8.96 10z" fill-rule="evenodd" />
+              </symbol>
+            </svg>
+
+            <form novalidate="novalidate" onsubmit="return false;" class="searchbox sbx-medium">
+              <div role="search" class="sbx-medium__wrapper">
+                <input type="search" name="search" placeholder="" autocomplete="off" required="required" class="sbx-medium__input search-bar">
+                <button type="submit" class="sbx-medium__submit search-img">
+                  <svg role="img" aria-label="Search">
+                    <use xlink:href="#sbx-icon-search-13"></use>
+                  </svg>
+                </button>
+                <button type="reset" title="Clear the search query." class="sbx-medium__reset">
+                  <svg role="img" aria-label="Reset">
+                    <use xlink:href="#sbx-icon-clear-2"></use>
+                  </svg>
+                </button>
+              </div>
+            </form>
           </div>
         </section>
         <section class="user-menu">
@@ -60,8 +83,10 @@ async function redirectPage() {
   if (getCookieValue(Keys.TOKEN_KEY)) {
     const user = await get(ApiUrl.USER_INFORMATION);
     if (user && user["role"] === "admin") {
-      $(".user-list").innerHTML = `<li class="admin">관리자페이지 |</li>
+      $(".user-list").innerHTML = `<li class="admin">관리자페이지</li>
+<span class="separator">/</span>
 <li class="logout">로그아웃</li>
+<span class="separator">/</span>
 <div class="basket">
   <img src="../img/shopping-bag.png" alt="cart-img">
 </div>`;
@@ -69,8 +94,10 @@ async function redirectPage() {
       $(".admin").addEventListener("click", () => (window.location.href = "/admin"));
       handleLogout();
     } else {
-      $(".user-list").innerHTML = `<li class="myPage">마이페이지 |</li>
+      $(".user-list").innerHTML = `<li class="myPage">마이페이지</li>
+<span class="separator">/</span>
 <li class="logout">로그아웃</li>
+<span class="separator">/</span>
 <div class="basket">
   <img src="../img/shopping-bag.png" alt="cart-img">
 </div>`;
