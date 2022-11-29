c78c627e797763065be8beac4887805d67999bb4
ahosang <sang0056@naver.com>
2022-11-29 23:18:52 +0900

    gitignore 수정

---
+++ testdata/comment.js
@@ -0,0 +1,68 @@
+const { productModel, userModel, commentModel } = require("../src/db");
+const { sampleComment } = require("./sample-comment");
+const { getRandomInt } = require("./get-random-int");
+const path = require("path");
+
+async function comment() {
+  try {
+    // 앞 상품 5개
+    const products = await productModel.findAll(0, 6);
+    const users = await userModel.findAll();
+    let comments = sampleComment.map((ment) => {
+      ment["productId"] = products[getRandomInt(0, 6)]._id;
+      ment["userId"] = users[getRandomInt(0, users.length)]._id;
+      return ment;
+    });
+
+    const fixedComments = await fixComment();
+    comments = comments.concat(fixedComments);
+    console.log(comments);
+    await commentModel.setTestdata(comments);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fixComment() {
+  const saveRoute = path.join("src", "views", "img", "comments");
+  const comments = [
+    {
+      productName: "고도리 복숭아 와인",
+      image: path.join(saveRoute, "고도리 복숭아 와인 후기"),
+      content:
+        "스파클링 + 달달한 복숭아 맛\n 상받았던 술이라고 해서 사봤는데 존맛\n도수 높은거 못드시는분 + 복숭아 좋아하시는분 무조건 드셔보세요:복숭아: ",
+    },
+    {
+      productName: "서울의 밤",
+      image: path.join(saveRoute, "서울의밤 후기"),
+      content:
+        "온더락으로 마시기 좋은 술:유리잔:\n하이볼로 만들어서도 많이 드시는것 같더라구요\n달지 않은 술 좋아하시는 분들에게 추천드려요:반짝임:",
+    },
+    {
+      productName: "황금 보리",
+      image: path.join(saveRoute, "황금보리 후기.jpg"),
+      content:
+        "보리향이 솔솔:벼_이삭:\n좋아하는 이자카야에 가져가서 먹었어요\n소주 특유의 역한 알콜향이 없어서 너무 좋습니다:결백:",
+    },
+    {
+      productName: "우주멜론미",
+      image: path.join(saveRoute, "우주멜론미.jpg"),
+      content: "저기요.. 우쥬 멜론 미?",
+    },
+  ];
+
+  const user = await userModel.findByEmail("sojin@elice.com");
+
+  const fixedComments = await Promise.all(
+    comments.map(async ({ productName, ...rest }) => {
+      const newComment = { ...rest };
+      const product = await productModel.findByObj({ name: productName });
+      newComment.productId = product._id;
+      newComment.userId = user._id;
+      return newComment;
+    })
+  );
+
+  return fixedComments;
+}
+module.exports = { comment };
---
+++ testdata/sample-comment.js
@@ -0,0 +1,34 @@
+const sampleComment = [
+  {
+    content: "와 이거 꿀맛입니다요",
+  },
+  {
+    content: "옴마마맘마 한병더",
+  },
+  {
+    content: "오늘 도착했는데 다 마시고 재주문이요",
+  },
+  {
+    content: "재주문이요!",
+  },
+  {
+    content: "이거 종류별로 재입고 되나요?",
+  },
+  {
+    content: "한잔만 마시려고 앉았는데, 꿀잠잤습니다.",
+  },
+  {
+    content: "이거 사줘 엄마",
+  },
+  {
+    content: "이거 사줘 아빠",
+  },
+  {
+    content: "저는 요즘 이거 보온병에 챙겨다녀요~",
+  },
+  {
+    content: "이거 들고 해운대가면 진짜 인기남",
+  },
+];
+
+module.exports = { sampleComment };
