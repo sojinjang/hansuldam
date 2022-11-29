08d72069f99adffae015033a332b287a07be4c89
sojinjang <jsojin6119@gmail.com>
2022-11-29 22:47:24 +0900

    :sparkles: feat(review): show the modification and deletion button only to the review writer

--- src/views/product_detail/review_board.js
+++ src/views/product_detail/review_board.js
@@ -1,6 +1,8 @@
 import * as api from "../api.js";
+import { getCookieValue } from "../utils/cookie.js";
 import { ApiUrl } from "../constants/ApiUrl.js";
 import { changeToKoreanTime } from "../utils/useful_functions.js";
+import { Keys } from "../constants/Keys.js";
 
 const emptyReview = document.querySelector(".empty-review");
 const reviewListContainer = document.querySelector(".review-list-container");
@@ -9,6 +11,7 @@ const queryString = new Proxy(new URLSearchParams(window.location.search), {
   get: (params, prop) => params.get(prop),
 });
 const productId = queryString.id;
+const userId = getCookieValue(Keys.USER_ID_KEY);
 
 async function getReviewList() {
   try {
@@ -32,17 +35,11 @@ function refineTitleAndDirectory(review) {
   return { reviewTitle, imgDirectory };
 }
 
-function showReview(review) {
-  const { reviewTitle, imgDirectory } = refineTitleAndDirectory(review);
-  const reviewContainer = document.createElement("div");
-  reviewContainer.setAttribute("class", "single-review-container");
-  reviewContainer.setAttribute("id", review._id);
-  let reviewElements = `
-              <div style='display:flex' class="single-review-title-container">
-                <div class="single-review-title-content">${reviewTitle}</div>
-  `;
-  if (imgDirectory) {
-    reviewElements += `
+function createReviewContent(reviewElements, review, imgDirectory) {
+  if (imgDirectory)
+    return (
+      reviewElements +
+      `
                 <div class="image-icon"><img src="../img/image-icon.png"></div>
                 <div class="single-review-title-username">${review.userId.fullName}</div>
             </div>
@@ -53,10 +50,11 @@ function showReview(review) {
               </div>
               <div class="review-content-detail">${review.content}</div>
               <div ><img class="review-img" src=${imgDirectory}></div>
-            </div>
-    `;
-  } else {
-    reviewElements += `
+              `
+    );
+  return (
+    reviewElements +
+    `
                 <div class="single-review-title-username">${review.userId.fullName}</div>
             </div>
             <div style='display:none' class="single-review-detail-container">
@@ -65,9 +63,37 @@ function showReview(review) {
                 <div class="review-username">${review.userId.fullName}</div>
               </div>
               <div class="review-content-detail">${review.content}</div>
-            </div>
-    `;
-  }
+    `
+  );
+}
+
+function createModificationDeletionButton(reviewElements, isWriter) {
+  if (isWriter)
+    return (
+      reviewElements +
+      `       <div class="writer-button-container">
+                <div class="review-modification">수정</div>
+                <div class="review-deletion">삭제</div>
+              </div>
+            </div>    
+    `
+    );
+  return reviewElements + `<div>`;
+}
+
+function showReview(review) {
+  const isWriter = userId === review.userId._id;
+  const { reviewTitle, imgDirectory } = refineTitleAndDirectory(review);
+  const reviewContainer = document.createElement("div");
+  reviewContainer.setAttribute("class", "single-review-container");
+  reviewContainer.setAttribute("id", review._id);
+  let reviewElements = `
+              <div style='display:flex' class="single-review-title-container">
+                <div class="single-review-title-content">${reviewTitle}</div>
+  `;
+  reviewElements = createReviewContent(reviewElements, review, imgDirectory);
+  reviewElements = createModificationDeletionButton(reviewElements, isWriter);
+
   reviewContainer.innerHTML = reviewElements;
   reviewListContainer.append(reviewContainer);
 }
